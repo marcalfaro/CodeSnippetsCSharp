30d1bc220858e68046548328c589835ca8d6efdc
Marc <marcalfaro@users.noreply.github.com>
2023-11-02 08:23:44 +1030

    Add project files.

---
+++ CodeSnippetsCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippetsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
